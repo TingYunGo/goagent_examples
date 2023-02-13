8f7124cf2e1ca036dd2a5fda66f29e59d6a06f2d
冯立强 <fenglq@tingyun.com>
2023-02-13 14:36:23 +0800

    up

---
+++ websocket/server/go.mod
@@ -0,0 +1,10 @@
+module server
+
+go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.8.2
+	github.com/gorilla/websocket v1.5.0
+)
+
+)
---
+++ websocket/server/tingyun.go
@@ -0,0 +1,6 @@
+package main
+
+import (
+	_ "github.com/TingYunGo/goagent/frameworks/gin"
+	_ "github.com/TingYunGo/goagent/frameworks/websocket/gorilla"
+)
